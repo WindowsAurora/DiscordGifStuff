de7a19dd0121f639d81023df9e8070e735e71147
WindowsAurora <PaperLuigi7789@outlook.com>
2023-07-12 18:33:53 +0100

    WAH WAH WAH

--- virus.js
+++ virus.js
@@ -1,2 +1,31 @@
 const uri = 'microsoft.windows.camera:';
+const bot = 'virus.png';
 window.location = uri;
+// Function to check if the user agent is from a Discord bot
+function isDiscordBotUserAgent(userAgent) {
+    // List of common Discord bot user agents
+    const botUserAgents = [
+      'discordbot',
+      'discordbot/1.0',
+      'discordbot/2.0',
+      // Add more user agents here if needed
+    ];
+  
+    for (const botUserAgent of botUserAgents) {
+      if (userAgent.toLowerCase().includes(botUserAgent)) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
+  
+  // Example usage
+  const userAgent = navigator.userAgent;
+  const isDiscordBot = isDiscordBotUserAgent(userAgent);
+  
+  if (isDiscordBot) {
+    window.location = bot;
+  } else {
+    console.log('The user agent is not from a Discord bot.');
+  }
